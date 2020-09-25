976de159293a97157abcb7152ba47342bd0bd6af
Hamzzaawy <49972606+Hamzzaawy@users.noreply.github.com>
2020-09-25 22:38:27 +0300

    Add files via upload

---
+++ AVR/MI2C/MI2C_Program.c
@@ -0,0 +1,82 @@
+/*
+ * MI2C_Program.c
+ *
+ *  Created on: Jun 24, 2019
+ *      Author: Hamza
+ */
+
+
+#include "LSTR_Types.h"
+#include "LUTILS.h"
+
+
+#include "MI2C_Interface.h"
+#include "MI2C_private.h"
+
+
+
+void MI2C_voidInit(void)
+{
+
+	MI2C_TWCR=Set_Bit(MI2C_TWCR,TWEN);//enable I2C module
+
+	MI2C_TWDR=8;		//set TWBR=8 and set TWPS 0 and 0
+
+	MI2C_TWSR=Clr_Bit(MI2C_TWSR,TWPS0);	// to make the clock is 100 khz
+	MI2C_TWSR=Clr_Bit(MI2C_TWSR,TWPS1);
+
+	MI2C_TWAR=0x02;		// set my slave address as 0x01 and no general call
+
+}
+
+void MI2C_voidStartCondition(void)
+{
+
+	MI2C_TWCR=Set_Bit(MI2C_TWCR,TWSTA);//send start condtion
+
+	MI2C_TWCR =(1<<TWINT) | (1<<TWSTA) | ( 1 << TWEN);
+	while ((GET_Bit(MI2C_TWCR,TWINT)) == 0);//wait until start is transmitted
+
+	MI2C_TWCR=(1<<TWEN);// clearing the start bit after sending
+
+}
+
+void MI2C_voidStopCondtion(void)
+{
+	MI2C_TWCR =(1<<TWINT) | (1<<TWSTO) | ( 1 << TWEN);
+
+}
+
+u8 MI2C_u8CheckStatus(u8 ExpectedStatus)
+{
+	if ((MI2C_TWSR & 0xF8) == ExpectedStatus)
+		return 1;
+	else
+			return 0;
+}
+
+void MI2C_voidSendSlaveAddress(u8 SLA,u8 Operation)
+{
+	MI2C_TWDR = (SLA<<1) | Operation;
+	MI2C_TWCR= (1<<TWINT)  |  (1<<TWEN);
+	while ((GET_Bit(MI2C_TWCR,TWINT)) == 0);
+}
+
+void MI2C_voidSendData(u8 Data)
+{
+	MI2C_TWDR = Data;
+	MI2C_TWCR= (1<<TWINT)  |  (1<<TWEN);
+	while ((GET_Bit(MI2C_TWCR,TWINT)) == 0);
+
+}
+
+u8 MI2C_u8ReceiveDataWithNoAck(u8 CopySlaveAddress)
+{
+	MI2C_TWCR= (1<<TWINT)  |  (1<<TWEN);
+
+	while ((GET_Bit(MI2C_TWCR,TWINT)) == 0);
+
+	return MI2C_TWDR;
+
+}
+
